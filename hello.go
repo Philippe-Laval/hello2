3fdbefbcc3ecc75e8658c04e539b931f59e83cf8
Philippe Laval <philippe.laval.1968@laposte.net>
2020-12-06 20:43:47 +0100

    gorm

--- hello.go
+++ hello.go
@@ -6,6 +6,9 @@ import (
 	"github.com/Philippe-Laval/stringutil"
 )
 
+// go get -u gorm.io/gorm
+// go get -u gorm.io/driver/sqlite
+
 func main() {
 	str := stringutil.Reverse("Hello Marina and Philippe.")
 	fmt.Println(str)
